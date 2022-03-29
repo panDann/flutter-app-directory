df46279f64e15e7230fe356dacd8be4395d3a1e3
liuhuangli <liuhuangli@mbcloud.com>
2022-03-29 16:20:29 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_maple
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/assest_path.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/cupertino.dart';
+
+const Map imageURL = {'girl' : 'assets/images/girl.jpg'};
